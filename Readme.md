83b36d765fbcf4d656e87520e4982daf73a7c803
Mark van Dijk <github@voidzero.net>
2022-10-25 10:23:05 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,60 @@
+# nixos-zfs-setup.sh
+
+## Hello, world!
+
+This is a script that sets up new zfs storage pools on your disk(s), for use
+with NixOS. It zaps the partition table(s), creates new partitions, sets up two
+storage pools (named `bpool` and `rpool` by default), and prepares them for
+NixOS installation.
+
+This is a proof of concept and not for beginners. You are free to modify the
+commands. The shell code shouldn't be too hard to understand.
+
+This script is based on the instructions from the [NixOS Root on
+ZFS](https://openzfs.github.io/openzfs-docs/Getting%20Started/NixOS/Root%20on%20ZFS.html)
+website, with a few deviations and exceptions:
+
+* It names the partitions in the GPT table it creates;
+* It uses `/dev/disk/partname` rather than `/dev/disk/by-id/*` or `/dev/disk/by-path/*`;
+* It (currently) does not support disk encryption;
+* It always allocates (but currently does not create and use) a swap partition.
+
+## How to use this script
+
+1.  Boot the NixOS liveCD
+2.  If not using DHCP, set up networking inside the live environment.
+3.  Open a terminal and login as root: `sudo login -f root`
+4.  Set a password (`passwd`).
+5.  Restart sshd: `systemctl restart sshd.service`
+6.  Login to the shell, `git clone` this repository
+7.  Edit the variables in `nixos-zfs-setup.sh`
+8.  Optional: wipe existing partitions from the disk with `wipefs -af /dev/...`
+9.  Run the script: `bash ./nixos-zfs-setup.sh` and hope for the best.
+
+When the script has finished, it will tell you some commands to run next. Do
+this by hand. Note down the instructions first, because the first command will
+output a lot of text.
+
+## Tested with:
+
+So far only with qemu. I plan to test it on hardware when I have a chance to.
+This may take a long time because I have plenty of other NixOS related stuff to
+figure out first.
+
+## Todo
+
+* Properly support/configure/enable the swap partition;
+* Add encryption.
+
+
+## Feedback
+
+Feedback is thankfully welcomed on the github issue tracker.
+
+## Blurb
+
+<sup><sub>Copyright © Mark van Dijk, 2022, The Netherlands.
+This product is licensed under the GPLv3. It comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it under certain
+conditions; see the LICENSE file for more information.</sub></sup>
+
---
+++ nixos-zfs-setup.sh
@@ -0,0 +1,250 @@
+#
+# Edit the variables below. Then run this by issuing:
+# `bash ./nixos-zfs-setup.sh`
+#
+# WARNING: this script will clear partition tables of existing disks.
+# Make sure that you set the DISK variable correctly. Additionally, it
+# is a good idea to clean your disk(s) current partitions first. Use the
+# command `wipefs` for this.
+#
+###############################################################################
+#    nixos-zfs-setup.sh: a bash script that sets up zfs disks for NixOS.      #
+#      Copyright (C) 2022  Mark (voidzero) van Dijk, The Netherlands.         #
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.   #
+###############################################################################
+
+set -ex
+
+DISK=(/dev/vda)
+#DISK=(/dev/vda /dev/vdb)
+
+# How to name the partitions. This will be visible in 'gdisk -l /dev/disk' and
+# in /dev/disk/by-partlabel.
+PART_MBR="bootcode"
+PART_EFI="efiboot"
+PART_BOOT="bpool"
+PART_SWAP="swap"
+PART_ROOT="rpool"
+
+# How much swap per disk?
+SWAPSIZE=2G
+
+# The type of virtual device for boot and root. If kept empty, the disks will
+# be joined (two 1T disks will give you ~2TB of data). Other valid options are
+# mirror, raidz types and draid types. You will have to manually add other
+# devices like spares, intent log devices, cache and so on. Here we're just
+# setting this up for installation after all.
+#ZFS_BOOT_VDEV="mirror"
+#ZFS_ROOT_VDEV="mirror"
+
+# How to name the boot pool and root pool.
+ZFS_BOOT="bpool"
+ZFS_ROOT="rpool"
+
+# How to name the root volume in which nixos will be installed.
+# If ZFS_ROOT is set to "rpool" and ZFS_ROOT_VOL is set to "nixos",
+# nixos will be installed in rpool/nixos, with a few extra subvolumes
+# (datasets).
+ZFS_ROOT_VOL="nixos"
+
+# Generate a root password with mkpasswd -m SHA-512
+ROOTPW=''
+
+# End of settings.
+
+set +x
+if [[ ${#DISK[*]} -eq 1 ]] && [[ -n ${ZFS_BOOT_VDEV} || -n ${ZFS_ROOT_VDEV} ]]
+then
+	echo "Error: You have only specified one disk. ZFS_BOOT_VDEV and ZFS_ROOT_DEV must be unset or empty." >&2
+	false
+fi
+
+if [[ -z ${ROOTPW} ]]
+then
+	echo "Error: Please generate a password hash and put that in this file's ROOTPW variable." >&2
+	false
+fi
+
+set -x
+
+i=0
+for d in ${DISK}
+do
+	sgdisk --zap-all ${d}
+	sgdisk -a1 -n1:0:+100K -t1:EF02 -c 1:${PART_MBR}${i} ${d}
+	sgdisk -n2:1M:+1G -t2:EF00 -c 2:${PART_EFI}${i} ${d}
+	sgdisk -n3:0:+4G -t3:BE00 -c 3:${PART_BOOT}${i} ${d}
+	sgdisk -n4:0:+${SWAPSIZE} -t4:8200 -c 4:${PART_SWAP}${i} ${d}
+	sgdisk -n5:0:0 -t5:BF00 -c 5:${PART_ROOT}${i} ${d}
+
+	partprobe ${d}
+	(( i++ )) || true
+done
+unset i d
+
+# Wait for a bit to let udev catch up and generate /dev/disk/by-partlabel.
+sleep 3s
+
+# Create the boot pool
+zpool create \
+	-o compatibility=grub2 \
+	-o ashift=12 \
+	-o autotrim=on \
+	-O acltype=posixacl \
+	-O compression=lz4 \
+	-O devices=off \
+	-O normalization=formD \
+	-O relatime=on \
+	-O xattr=sa \
+	-O mountpoint=none \
+	-O checksum=sha256 \
+	-R /mnt \
+	${ZFS_BOOT} ${ZFS_BOOT_VDEV} /dev/disk/by-partlabel/${PART_BOOT}*
+
+# Create the root pool
+zpool create \
+	-o ashift=12 \
+	-o autotrim=on \
+	-O acltype=posixacl \
+	-O compression=zstd \
+	-O dnodesize=auto -O normalization=formD \
+	-O relatime=on \
+	-O xattr=sa \
+	-O mountpoint=none \
+	-O checksum=edonr \
+	-R /mnt \
+	${ZFS_ROOT} ${ZFS_ROOT_VDEV} /dev/disk/by-partlabel/${PART_ROOT}*
+
+
+# Create datasets (subvolumes) in the root dataset
+zfs create -o mountpoint=/     ${ZFS_ROOT}/${ZFS_ROOT_VOL}
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/home
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/nix
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/var
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/var/lib
+zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/var/log
+zfs create ${ZFS_BOOT}/${ZFS_ROOT_VOL}
+
+# Create datasets (subvolumes) in the boot dataset
+zfs create -o mountpoint=/boot ${ZFS_BOOT}/${ZFS_ROOT_VOL}/boot
+
+# Create, mount and populate the efi partitions
+i=0
+for d in ${DISK}
+do
+	mkfs.vfat -n EFI /dev/disk/by-partlabel/${PART_EFI}${i}
+	mkdir -p /mnt/boot/efis/${PART_EFI}${i}
+	mount -t vfat /dev/disk/by-partlabel/${PART_EFI}${i} /mnt/boot/efis/${PART_EFI}${i}
+	(( i++ )) || true
+done
+unset i d
+
+# Mount the first drive's efi partition to /mnt/boot/efi
+mkdir /mnt/boot/efi
+mount -t vfat /dev/disk/by-partlabel/${PART_EFI}0 /mnt/boot/efi
+
+# Make sure we won't trip over zpool.cache later
+mkdir -p /mnt/etc/zfs/
+rm -f /mnt/etc/zfs/zpool.cache
+touch /mnt/etc/zfs/zpool.cache
+chmod a-w /mnt/etc/zfs/zpool.cache
+chattr +i /mnt/etc/zfs/zpool.cache
+
+# Generate and edit configs
+nixos-generate-config --root /mnt
+
+sed -i -e "s|./hardware-configuration.nix|& ./zfs.nix|" /mnt/etc/nixos/configuration.nix
+
+tee -a /mnt/etc/nixos/zfs.nix <<EOF
+{ config, pkgs, ... }:
+
+{
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "$(head -c 8 /etc/machine-id)";
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.zfs.devNodes = "/dev/disk/by-partlabel";
+EOF
+
+# Remove boot.loader stuff, it's to be added to zfs.nix
+sed -i '/boot.loader/d' /mnt/etc/nixos/configuration.nix
+
+# Disable xserver. Comment them without a space after the pound sign so we can
+# recognize them when we edit the config later
+sed -i -e 's;^  \(services.xserver\);  #\1;' /mnt/etc/nixos/configuration.nix
+
+tee -a /mnt/etc/nixos/zfs.nix <<-'EOF'
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.generationsDir.copyKernels = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.copyKernels = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.zfsSupport = true;
+
+  boot.loader.grub.extraPrepareConfig = ''
+    mkdir -p /boot/efis
+    for i in  /boot/efis/*; do mount $i ; done
+
+    mkdir -p /boot/efi
+    mount /boot/efi
+  '';
+
+  boot.loader.grub.extraInstallCommands = ''
+    ESP_MIRROR=$(mktemp -d)
+    cp -r /boot/efi/EFI $ESP_MIRROR
+    for i in /boot/efis/*; do
+      cp -r $ESP_MIRROR/EFI $i
+    done
+    rm -rf $ESP_MIRROR
+  '';
+
+  boot.loader.grub.devices = [
+EOF
+
+for d in ${DISK}; do
+  printf "    \"${d}\"\n" >>/mnt/etc/nixos/zfs.nix
+done
+
+tee -a /mnt/etc/nixos/zfs.nix <<EOF
+  ];
+
+EOF
+
+sed -i 's|fsType = "zfs";|fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];|g' \
+/mnt/etc/nixos/hardware-configuration.nix
+
+ADDNR=$(awk '/^  fileSystems."\/" =$/ {print NR+3}' /mnt/etc/nixos/hardware-configuration.nix)
+sed -i "${ADDNR}i"' \      neededForBoot = true;' /mnt/etc/nixos/hardware-configuration.nix
+
+ADDNR=$(awk '/^  fileSystems."\/boot" =$/ {print NR+3}' /mnt/etc/nixos/hardware-configuration.nix)
+sed -i "${ADDNR}i"' \      neededForBoot = true;' /mnt/etc/nixos/hardware-configuration.nix
+
+tee -a /mnt/etc/nixos/zfs.nix <<EOF
+users.users.root.initialHashedPassword = "${ROOTPW}";
+
+}
+EOF
+
+set +x
+
+echo "Now do this:"
+echo "nixos-install -v --show-trace --no-root-passwd --root /mnt"
+echo "umount -Rl /mnt"
+echo "zpool export -a"
+echo "reboot"
+echo "Make note of these instructions because the nixos-install command will output a lot of text."
+
